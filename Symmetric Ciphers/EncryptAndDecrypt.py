cb9dda28d18ac4baecf3048c5c3912b5a598901f
Brandon Hough <brandonhough24@yahoo.com>
2019-03-07 22:54:53 -0500

    first commit to setup assignment

--- Symmetric Ciphers/EncryptAndDecrypt.py
+++ Symmetric Ciphers/EncryptAndDecrypt.py
@@ -9,7 +9,26 @@ series of bytes, and decryption of the created cipher text"""
 # =============================================================
 # Imports
 # =============================================================
+import re
+from Crypto.Cipher import AES
+import base64
+import os
 
-# =============================================================
-# Code Begins Here
-# =============================================================
+def validate_input(input_str):
+    if not re.match("^[a-z]*$", input_str):
+        print("Please enter only characters!")
+    else:
+        decrypt_using_des(input_str)
+
+def getUserInput():
+    plain_text_string = input()
+    validate_input(plain_text_string)
+
+def decrypt_using_des(input_str):
+    print("okay..")
+
+def main():
+    getUserInput()
+
+if __name__ == '__main__':
+    main()
