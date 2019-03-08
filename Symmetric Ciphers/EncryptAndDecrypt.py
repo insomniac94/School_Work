8fbcf23b5a910b9a796b5e53b941a987ccca1619
Brandon Hough <brandonhough24@yahoo.com>
2019-03-08 18:37:14 -0500

    minor changes

--- Symmetric Ciphers/EncryptAndDecrypt.py
+++ Symmetric Ciphers/EncryptAndDecrypt.py
@@ -9,26 +9,61 @@ series of bytes, and decryption of the created cipher text"""
 # =============================================================
 # Imports
 # =============================================================
-import re
-from Crypto.Cipher import AES
 import base64
 import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import pyDes
 
-def validate_input(input_str):
-    if not re.match("^[a-z]*$", input_str):
-        print("Please enter only characters!")
-    else:
-        decrypt_using_des(input_str)
+def encrypt_decrypt_des(input_str):
 
-def getUserInput():
-    plain_text_string = input()
-    validate_input(plain_text_string)
+    print("\nDES ENCRYPTION")
+    print("="*24)
 
-def decrypt_using_des(input_str):
-    print("okay..")
+    key = pyDes.des("DESCRYPT", pyDes.CBC, "\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5)
+    decrypted_msg = key.encrypt(input_str)
+    #print("Key: %r" % key)
+    print("Encrypted: %r" % decrypted_msg)
+    print("Decrypted: %r" % key.decrypt(decrypted_msg))
+
+    '''
+    salt = os.urandom(16)
+    kdf = PBKDF2HMAC(
+        algorithm = hashes.SHA256(),
+        length = 32,
+        salt = salt,
+        iterations = 100000,
+        backend = default_backend()
+    )
+
+    password = b'testpassword'
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+
+    print("Key: ")
+    print(key)
+    
+    f = Fernet(key)
+
+    token = f.encrypt(input_str)
+    print("\nEncrypted Message: ")
+    print(token)
+
+    print("\nDecrypted Message: ")
+    print(f.decrypt(token))
+    '''
+
+def encrypt_decrypt_aes(input_str):
+    print("\nAES ENCRYPTION")
+    print("="*24)
+
 
 def main():
-    getUserInput()
+    plain_text_msg = input('\nPlease input text to encrypt: ')
+    plain_text_msg = str.encode(plain_text_msg)
+    encrypt_decrypt_des(plain_text_msg)
+    encrypt_decrypt_aes(plain_text_msg)
 
 if __name__ == '__main__':
     main()
